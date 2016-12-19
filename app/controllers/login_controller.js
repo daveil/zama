143b3461adf1cc4b13c4d25b2bac727c1b4bf284
Dave Arroyo <arroyo.daveil@gmail.com>
2016-12-19 16:44:18 +0800

    Registration form

--- app/controllers/login_controller.js
+++ app/controllers/login_controller.js
@@ -1,6 +1,35 @@
 "use strict";
 define(['app','api'], function (app) {
-    app.register.controller('LoginController',['$scope','$rootScope','$window','$cookies','api', function ($scope,$rootScope,$window,$cookies,api) {
+    
+	app.register.controller('RegisterController',['$scope','$rootScope','$window','$cookies','api', function ($scope,$rootScope,$window,$cookies,api) {
+		$scope.Register = {};
+		$scope.toggleRegister = function(){
+			$rootScope.__SHOW_REG = true;
+		}
+		$scope.cancel = function(){
+			$scope.Register = {};
+			$rootScope.__SHOW_REG = false;
+		}
+		$scope.register = function(){
+			var data =  $scope.Register;
+				data.action = 'register';
+				$scope.Registering = true;
+			api.POST('users',data,function(response){
+				$scope.Registering = false;
+				if(response.data){
+					$cookies.put('__USER',JSON.stringify(response.data));
+					$window.location.href="#/";
+				}
+			});
+		}
+		api.GET('departments',function(response){
+			$rootScope.Departments = response.data;
+		});
+	}]);
+	
+	
+	app.register.controller('LoginController',['$scope','$rootScope','$window','$cookies','api', function ($scope,$rootScope,$window,$cookies,api) {
+		$rootScope.__SHOW_REG = false;
 		if($window.location.hash=='#/logout'){
 			$rootScope.__SIDEBAR_OPEN = false;
 			$cookies.remove('__USER');
@@ -15,12 +44,10 @@ define(['app','api'], function (app) {
 		}
 		$scope.cancel();
 		$scope.login = function(){
-			var data = {
-				username:$scope.User.name,
-				password:$scope.User.password,
-			};
+			var data = $scope.User;
+				data.action='login';
 			$scope.LoggingIn = true;
-			api.POST('login',data,function(response){
+			api.POST('users',data,function(response){
 				$scope.LoggingIn = false;
 				if(response.data.user){
 					$cookies.put('__USER',JSON.stringify(response.data.user));
