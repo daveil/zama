08af5598404eac22cad670d1928b97fbe686360c
Dave Arroyo <arroyo.daveil@gmail.com>
2016-12-26 18:43:57 +0800

    Add inputbox and dropdown data for registration

--- controllers/system_users_controller.js
+++ controllers/system_users_controller.js
@@ -1,6 +1,19 @@
 "use strict";
 define(['app','api'], function (app) {
-    app.register.controller('AccessController',['$scope','$rootScope','api', function ($scope,$rootScope,api) {
+	//SystemUsers.Register
+    app.register.controller('RegisterController',['$scope','$rootScope','api', function ($scope,$rootScope,api) {
+		$scope.init = function(){
+			api.GET('groups',function(response){
+				$scope.Restriction = response.data;
+			});
+			api.GET('departments',function(response){
+				$scope.Departments = response.data;	
+			});			
+		};
+		
+	}]);
+	//SystemUsers.Access
+	app.register.controller('AccessController',['$scope','$rootScope','api', function ($scope,$rootScope,api) {
 		$scope.Access = {};
 		api.GET('groups',function(response){
 			$scope.Groups = response.data;
