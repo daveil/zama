20cc9b4d595e9276349e1f7ab62ad688fd807e30
Dave Alaras <arroyo.daveil@gmail.com>
2016-12-28 11:16:53 +0800

    Implement pareto filtering for encoding

--- controllers/pareto_controller.js
+++ controllers/pareto_controller.js
@@ -2,16 +2,42 @@
 define(['app','api'], function (app) {
     app.register.controller('IndividualController',['$scope','$rootScope','api', function ($scope,$rootScope,api) {
     	var dept  = $rootScope.__USER.department;
-    	api.GET('departments',{id:dept},function(response){
-    		$scope.Department = response.data[0];
-    		var data  ={department_id:dept};
-    		api.GET('categories',data,function(response){
-    			$scope.Categories = response.data;
-    		});
-    		api.GET('subcategories',data,function(response){
-    			$scope.SubCategories = response.data;
-    		});
-    	});
+        var data  ={department_id:dept};
+        function getData(type){
+            switch(type){
+                case 'dept':
+                    api.GET('departments',{id:dept},function(response){
+                        $scope.Department =  response.data[0];
+                        getData('cat');
+                        getData('subcat');
+                    });
+                break;
+                case 'cat':
+                    api.GET('categories',data,function(response){
+                        $scope.Categories = response.data;
+                    });
+                break;
+                case 'subcat':
+                   api.GET('subcategories',data,function(response){
+                    $scope.SubCategories = response.data;
+                    });
+                break;
+                case 'lnmn':
+                   api.GET('linemachines',data,function(response){
+                    $scope.LineMachines = response.data;
+                    });
+                break;
+            }
+        }
+        getData('dept');
+        $scope.$watch('Category',function(){
+            data.category_id =  $scope.Category;
+            getData('subcat');
+        });
+        $scope.$watch('SubCategory',function(){
+            data.subcategory_id =  $scope.SubCategory;
+            getData('lnmn');
+        });
 
     }]);
 });
