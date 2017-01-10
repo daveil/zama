5bacdd98b97dc6617a08aa1097021cc65918e425
Dave Arroyo <arroyo.daveil@gmail.com>
2017-01-10 11:46:00 +0800

    Ignore cache folder and resolve code

--- api/models/department.php
+++ api/models/department.php
@@ -1,7 +1,7 @@
 <?php
 class Department extends AppModel {
 	var $name = 'Department';
-	var $primaryKey = 'code';
+	
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
 
 	var $hasMany = array(
--- api/models/kpi.php
+++ api/models/kpi.php
@@ -1,7 +1,7 @@
 <?php
 class Kpi extends AppModel {
 	var $name = 'Kpi';
-	var $primaryKey = 'code';
+	
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
 
 	var $belongsTo = array(
