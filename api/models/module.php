e92c0fddfb5751778efbac5808cd50506035ee3b
Dave Arroyo <arroyo.daveil@gmail.com>
2017-01-16 03:51:52 +0800

    Filter module base on group

--- api/models/module.php
+++ api/models/module.php
@@ -2,4 +2,13 @@
 class Module extends AppModel {
 	var $name = 'Module';
 	var $actsAs = array('Tree');
+	var $hasMany = array(
+		'Right' => array(
+			'className' => 'Right',
+			'foreignKey' => 'module_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
 }
