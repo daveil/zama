c30e0d6a1601d7459706b7b8f91378d1fb82c1e6
Dave Arroyo <arroyo.daveil@gmail.com>
2017-01-22 12:16:07 +0800

    Adjust pareto dropdown filter

--- controllers/pareto_controller.js
+++ controllers/pareto_controller.js
@@ -10,77 +10,90 @@ define(['app','api'], function (app) {
 			$scope.Submitting=false;
 			$scope.PreventCancel = true;
 			$scope.PreventSubmit = true;
+			getData('dept');
 		}
          function getData(type,data){
             switch(type){
                 case 'dept':
-                    var data = {};
+					var data = {};
 					if(dept!='all')
 						data.id = dept;
                     api.GET('departments',data,function(response){
 						$scope.Departments = response.data;
 						$scope.Department = {};
 						if(dept!='all')
-							$scope.Department =  response.data[0];
-						getData('cat',data);
-                       
+							$scope.Department =  response.data[0].id;
+						else
+							$scope.Department = null;
                     });
                 break;
                 case 'cav':
-                    api.GET('cavities',data,function(response){
+                    return api.GET('cavities',data,function(response){
                         $scope.Cavities = response.data;
                     });
                 break;
                 case 'cat':
-                    api.GET('categories',data,function(response){
+                    return api.GET('categories',data,function(response){
                         $scope.Categories = response.data;
                     });
                 break;
                 case 'kpi':
-                   api.GET('kpis',data,function(response){
+                   return api.GET('kpis',data,function(response){
                     $scope.KPIs = response.data;
                     });
                 break;
                 case 'subcat':
-                   api.GET('subcategories',data,function(response){
+                   return api.GET('subcategories',data,function(response){
                     $scope.SubCategories = response.data;
                     });
                 break;
                 case 'lnmn':
-                   api.GET('line_machines',data,function(response){
+                   return api.GET('line_machines',data,function(response){
                     $scope.LineMachines = response.data;
-					$scope.PreventCancel = false;
-					$scope.PreventSubmit = false;
                     });
                 break;
 				case 'mod':
-                    api.GET('model_nos',data,function(response){
+                    return api.GET('model_nos',data,function(response){
                         $scope.Models = response.data;
-						for(var i in $scope.Models ){
-							var model =  $scope.Models[i];
-							$scope.ParetoDetail[i] = {};
-							$scope.ParetoDetail[i].model_no_id =  model.id;
-						}
                     });
                 break;
             }
         }
-       getData('dept');
-       $scope.$watch('Category',function(){
-            getData('kpi',{category_id:$scope.Category});
-        });
-        $scope.$watch('KPI',function(){
-            getData('subcat',{kpi_id:$scope.KPI});
-        });
-		$scope.$watch('SubCategory',function(){
-            getData('lnmn',{subcategory_id:$scope.SubCategory});
-        });
-		$scope.byParentObj = function(id,field){
-			return true;
-			return function(item){
-				return item[field]==id;
+		$scope.$watch('Department',function(value){
+			$scope.Category = null;
+			$scope.Categories = [];
+			$scope.KPIs = [];
+			$scope.SubCategories = [];
+			$scope.LineMachines = [];
+			if($scope.Department){
+				var data = {department_id:$scope.Department};
+				var models = 'Categories|KPIs|SubCategories|LineMachines';
+					models = models.split('|');
+				var list = 'cat|kpi|subcat|lnmn';
+					list = list.split('|');
+				(function requestList(index,data){
+					console.log(index);
+					if(index<list.length)
+						getData(list[index],data).then(
+						function success(){
+							return requestList(index+1,data)
+							},
+						function error(){
+							$scope[models[index]] = [{}];
+							return requestList(index+1,data)
+							}
+						);
+				})(0,data);
 			}
-		}
+		});
+		$scope.$watchGroup(['Category','KPI','SubCategory','LineMachine'],function(){
+			if(!$scope.Category)
+				$scope.KPI = null;
+			if(!$scope.KPI)
+				$scope.SubCategory = null;
+			if(!$scope.SubCategory)
+				$scope.LineMachine = null;
+		});
 		$scope.updateModel = function(){
 			//if($scope.LineMachine){
 				$scope.Models = [];
